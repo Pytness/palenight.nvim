5c7a7405df306a86ef7bc68aaf11ae6348deee76
Fernando Vega <pytness@gmail.com>
2025-09-25 10:55:09 +0200

    feat: update term colors

--- lua/palenight/term.lua
+++ lua/palenight/term.lua
@@ -4,19 +4,21 @@ local M = {}
 
 M.set_terminal_colors = function()
   vim.g.terminal_color_0 = colors.black
+
   vim.g.terminal_color_1 = colors.red
-  vim.g.terminal_color_2 = colors.green
-  vim.g.terminal_color_3 = colors.yellow
-  vim.g.terminal_color_4 = colors.blue
+  vim.g.terminal_color_2 = colors.darK_green
+  vim.g.terminal_color_3 = colors.dark_yellow
+  vim.g.terminal_color_4 = colors.dark_blue
   vim.g.terminal_color_5 = colors.purple
-  vim.g.terminal_color_6 = colors.cyan
+  vim.g.terminal_color_6 = colors.dark_cyan
   vim.g.terminal_color_7 = colors.white
+
   vim.g.terminal_color_8 = colors.visual_grey
-  vim.g.terminal_color_9 = colors.dark_red
+  vim.g.terminal_color_9 = colors.light_red
   vim.g.terminal_color_10 = colors.green
-  vim.g.terminal_color_11 = colors.dark_yellow
+  vim.g.terminal_color_11 = colors.yellow
   vim.g.terminal_color_12 = colors.blue
-  vim.g.terminal_color_13 = colors.purple
+  vim.g.terminal_color_13 = colors.light_purple
   vim.g.terminal_color_14 = colors.cyan
   vim.g.terminal_color_15 = colors.comment_grey
   vim.g.terminal_color_background = colors.black
